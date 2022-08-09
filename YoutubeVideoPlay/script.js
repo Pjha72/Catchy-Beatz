453cc04de49970b8767477c46df1046cef87377b
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-08-09 14:58:31 +0530

    Added YouTube API

---
+++ YoutubeVideoPlay/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    var API_KEY = "AIzaSyBVKHyGXAnVWRlIoW7edGHAFe95unPZxGs"
+    var video = ""
+
+    $("form").submit(function () {
+        event.preventDefault()
+
+        var search = $('#search').val()
+
+        videoSearch(API_KEY, search, 9)
+    })
+
+    function videoSearch(key, search, maxResults) {
+
+        $("#videos").empty()
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key
+            + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function (data) {
+                console.log(data)
+
+
+                data.items.forEach(item => {
+                    video = `
+                <iframe src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>              
+                
+                `
+                    $("#videos").append(video)
+                });
+            })
+    }
+
+})
