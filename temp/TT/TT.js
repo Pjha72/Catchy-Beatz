5b662c7f9a4c541dcfa1d40a9b3f83029d401b16
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-09-21 11:05:00 +0530

    Add files via upload

--- temp/TT/TT.js
+++ temp/TT/TT.js
@@ -22,10 +22,10 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'TT.jpg',
-        name : 'TT',
-        artist : 'Twice',
-        music : 'TT.mp3'
+        img: 'TT.jpg',
+        name: 'TT',
+        artist: 'Twice',
+        music: 'TT.mp3'
     }
     // ,
     // {
@@ -50,7 +50,7 @@ const music_list = [
 
 loadTrack(track_index);
 
-function loadTrack(track_index){
+function loadTrack(track_index) {
     clearInterval(updateTimer);
     reset();
 
@@ -72,53 +72,53 @@ function loadTrack(track_index){
 //     let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
 //     let a;
 
-    // function populate(a){
-    //     for(let i=0; i<6; i++){
-    //         let x = Math.round(Math.random() * 14);
-    //         let y = hex[x];
-    //         a += y;
-    //     }
-    //     return a;
-    // }
-    // let Color1 = populate('#');
-    // let Color2 = populate('#');
-    // var angle = 'to right';
-
-    // let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
-    // document.body.style.background = gradient;
+// function populate(a){
+//     for(let i=0; i<6; i++){
+//         let x = Math.round(Math.random() * 14);
+//         let y = hex[x];
+//         a += y;
+//     }
+//     return a;
 // }
-function reset(){
+// let Color1 = populate('#');
+// let Color2 = populate('#');
+// var angle = 'to right';
+
+// let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+// document.body.style.background = gradient;
+// }
+function reset() {
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
 }
-function randomTrack(){
+function randomTrack() {
     isRandom ? pauseRandom() : playRandom();
 }
-function playRandom(){
+function playRandom() {
     isRandom = true;
     randomIcon.classList.add('randomActive');
 }
-function pauseRandom(){
+function pauseRandom() {
     isRandom = false;
     randomIcon.classList.remove('randomActive');
 }
-function repeatTrack(){
+function repeatTrack() {
     let current_index = track_index;
     loadTrack(current_index);
     playTrack();
 }
-function playpauseTrack(){
+function playpauseTrack() {
     isPlaying ? pauseTrack() : playTrack();
 }
-function playTrack(){
+function playTrack() {
     curr_track.play();
     isPlaying = true;
     track_art.classList.add('rotate');
     wave.classList.add('loader');
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
-function pauseTrack(){
+function pauseTrack() {
     curr_track.pause();
     isPlaying = false;
     track_art.classList.remove('rotate');
@@ -146,16 +146,16 @@ function pauseTrack(){
 //     loadTrack(track_index);
 //     playTrack();
 // }
-function seekTo(){
+function seekTo() {
     let seekto = curr_track.duration * (seek_slider.value / 100);
     curr_track.currentTime = seekto;
 }
-function setVolume(){
+function setVolume() {
     curr_track.volume = volume_slider.value / 100;
 }
-function setUpdate(){
+function setUpdate() {
     let seekPosition = 0;
-    if(!isNaN(curr_track.duration)){
+    if (!isNaN(curr_track.duration)) {
         seekPosition = curr_track.currentTime * (100 / curr_track.duration);
         seek_slider.value = seekPosition;
 
@@ -164,12 +164,20 @@ function setUpdate(){
         let durationMinutes = Math.floor(curr_track.duration / 60);
         let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
 
-        if(currentSeconds < 10) {currentSeconds = "0" + currentSeconds; }
-        if(durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if(currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
-        if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
         total_duration.textContent = durationMinutes + ":" + durationMinutes;
     }
+}
+
+
+// My Code
+export function NamesOfPlays() {
+    let name =  music_list[track_index].name + " " +  music_list[track_index].artist;
+    console.log(name);
+    return 0;
 }
---
+++ temp/play.js
@@ -0,0 +1,8 @@
+// let trackName = document.getElementsByClassName("track-name")[0];
+// let trackArtist = document.getElementsByClassName("track-artist")[0];
+
+// var search = 
+import { hello } from 'TT/TT.js';
+let val = NamesOfPlays();  // val is "Hello";
+
+let trackName = music_list[track_index].name;
